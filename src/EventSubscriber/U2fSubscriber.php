75f3a5c35450932920de1587763c979ba45c710f
Michael Barbey <mbarbey@sqli.com>
2018-10-25 14:33:08 +0200

    working on demo

--- src/EventSubscriber/U2fSubscriber.php
+++ src/EventSubscriber/U2fSubscriber.php
@@ -6,31 +6,42 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Mbarbey\U2fSecurityBundle\Event\Authentication\U2fAuthenticationFailureEvent;
 use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
-use Mbarbey\U2fSecurityBundle\Event\Authentication\U2fPreAuthenticationEvent;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 
 class U2fSubscriber implements EventSubscriberInterface
 {
     private $flash;
     private $session;
+    private $router;
 
-    public function __construct(FlashBagInterface $flash, SessionInterface $session)
+    public function __construct(FlashBagInterface $flash, SessionInterface $session, RouterInterface $router)
     {
         $this->flash = $flash;
         $this->session = $session;
+        $this->router = $router;
     }
 
     public static function getSubscribedEvents()
     {
         return [
             U2fAuthenticationFailureEvent::getName() => 'onU2fAuthenticationFailure',
-            U2fPreAuthenticationEvent::getName() => 'onPreAuthentication'
+            KernelEvents::REQUEST => 'onKernelRequest'
         ];
     }
 
-    public function onPreAuthentication(U2fPreAuthenticationEvent $event)
+    public function onKernelRequest(GetResponseEvent $event)
     {
-        if ($this->session->get('u2f_registration_error_counter', 0) >= 3) {
-            $this->flash->add('danger', "You failed to authenticate yourself and now you try to leave ? Mouahaha you are jailed here forever ! Or you can just logout and try again...");
+        if (
+            $event->isMasterRequest() &&
+            $event->getRequest()->getSession()->get('u2f_registration_error_counter', 0) > 0
+        ) {
+            $route = $event->getRequest()->get('_route');
+            $whitelist = ['login', 'logout', 'user_authenticate_u2f'];
+            if (!in_array($route, $whitelist) && substr($route, 0, 1) != '_') {
+                $this->flash->add('warning', "Mouahaha, you are not allowed to leave ! Except if you successfully authenticate or if you logout...");
+            }
         }
     }
 
